2700754793bc303e929b98c96610039d654071ee
Don Drake <ddrake@dreamingmind.com>
2015-01-15 14:00:53 -0800

    comments

--- Controller/AddressModuleAppController.php
+++ Controller/AddressModuleAppController.php
@@ -4,6 +4,11 @@ App::uses('AppController', 'Controller');
 
 /**
  * CakePHP AddressModuleAppController
+ * 
+ * A general purpose Address tool for storing, editing and displaying address
+ * which can be linked to any other model. The Address Model schema carries 
+ * both an alias and id to allow this linking.
+ * 
  * @author dondrake
  */
 class AddressModuleAppController extends AppController {
